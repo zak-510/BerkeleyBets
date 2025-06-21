e878d4d45348c9accaf18c6ac3eaf167138405bc
Jeremiah de Guzman <124826126+jerrymiahdg@users.noreply.github.com>
2025-06-21 16:45:27 -0700

    some design changes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
   const auth = getAuth();
 
   return (
-    <div className=" index w-full flex items-center border-b">
+    <div className=" index w-full flex items-center border-b border-white/25">
       <div className=" z-50 p-5 max-w-5xl w-full flex justify-between">
         <div className="flex justify-between gap-5">
           <img className="w-10 object-contain" src={CalLogo}></img>
